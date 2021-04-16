6567c30ebd0dc2a530535957ca56c4c9ab0652ac
Johudo <dimazuev11@gmail.com>
2021-04-17 00:04:09 +0300

    Fixed frontend

--- static/js/main.js
+++ static/js/main.js
@@ -1,98 +1,95 @@
-$('body').on('click', '.showPasswordToggleOne', function(){
-      if ($('#password-input').attr('type') == 'password'){
-        $(this).addClass('view');
-        $('#password-input').attr('type', 'text');
-      } else {
-        $(this).removeClass('view');
-        $('#password-input').attr('type', 'password');
-      }
-      return false;
+$("body").on("click", ".showPasswordToggleOne", function () {
+    if ($("#password-input").attr("type") == "password") {
+        $(this).addClass("view");
+        $("#password-input").attr("type", "text");
+    } else {
+        $(this).removeClass("view");
+        $("#password-input").attr("type", "password");
+    }
+    return false;
 });
-$('body').on('click', '.showPasswordToggleTwo', function(){
-      if ($('#password-input').attr('type') == 'password'){
-        $(this).addClass('view');
-        $('#password-input').attr('type', 'text');
-      } else {
-        $(this).removeClass('view');
-        $('#password-input').attr('type', 'password');
-      }
-      return false;
-});
-
-$(document).ready(function() {
-  $('a.myLinkModal').click( function(event){
-    event.preventDefault();
-    $('#myOverlay').fadeIn(297, function(){
-      $('#myModal') 
-      .css('display', 'block')
-      .animate({opacity: 1}, 198);
-    });
-  });
-
-  $('#myModal__close, #myOverlay').click( function(){
-    $('#myModal').animate({opacity: 0}, 198,
-      function(){
-        $(this).css('display', 'none');
-        $('#myOverlay').fadeOut(297);
-    });
-  });
+$("body").on("click", ".showPasswordToggleTwo", function () {
+    if ($("#password-input").attr("type") == "password") {
+        $(this).addClass("view");
+        $("#password-input").attr("type", "text");
+    } else {
+        $(this).removeClass("view");
+        $("#password-input").attr("type", "password");
+    }
+    return false;
 });
 
-$(document).ready(function() {
-  $('a.myLinkModal1').click( function(event){
-    event.preventDefault();
-    $('#myOverlay1').fadeIn(297, function(){
-      $('#myModal1') 
-      .css('display', 'block')
-      .animate({opacity: 1}, 198);
+$(document).ready(function () {
+    $("a.myLinkModal").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#myModal").css("display", "block").animate({ opacity: 1 }, 198);
+        });
     });
-  });
 
-  $('#myModal__close1, #myOverlay1').click( function(){
-    $('#myModal1').animate({opacity: 0}, 198,
-      function(){
-        $(this).css('display', 'none');
-        $('#myOverlay1').fadeOut(297);
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#myModal").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
     });
-  });
 });
 
-$(document).ready(function() {
-  $('a.myLinkModal2').click( function(event){
-    event.preventDefault();
-    $('#myOverlay2').fadeIn(297, function(){
-      $('#myModal2') 
-      .css('display', 'block')
-      .animate({opacity: 1}, 198);
+$(document).ready(function () {
+    $("a.myLinkModal1").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay1").fadeIn(297, function () {
+            $("#myModal1").css("display", "block").animate({ opacity: 1 }, 198);
+        });
     });
-  });
 
-  $('#myModal__close2, #myOverlay2').click( function(){
-    $('#myModal2').animate({opacity: 0}, 198,
-      function(){
-        $(this).css('display', 'none');
-        $('#myOverlay2').fadeOut(297);
+    $("#myModal__close1, #myOverlay1").click(function () {
+        $("#myModal1").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay1").fadeOut(297);
+        });
     });
-  });
 });
 
-$(document).ready(function() {
-  $('a.myLinkModal3').click( function(event){
-    event.preventDefault();
-    $('#myOverlay3').fadeIn(297, function(){
-      $('#myModal3') 
-      .css('display', 'block')
-      .animate({opacity: 1}, 198);
+$(document).ready(function () {
+    $("a.myLinkModal2").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay2").fadeIn(297, function () {
+            $("#myModal2").css("display", "block").animate({ opacity: 1 }, 198);
+        });
     });
-  });
 
-  $('#myModal__close3, #myOverlay3').click( function(){
-    $('#myModal3').animate({opacity: 0}, 198,
-      function(){
-        $(this).css('display', 'none');
-        $('#myOverlay3').fadeOut(297);
+    $("#myModal__close2, #myOverlay2").click(function () {
+        $("#myModal2").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay2").fadeOut(297);
+        });
     });
-  });
+});
+
+$(document).ready(function () {
+    $("a.myLinkModal3").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay3").fadeIn(297, function () {
+            $("#myModal3").css("display", "block").animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close3, #myOverlay3").click(function () {
+        $("#myModal3").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay3").fadeOut(297);
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#myModal__close4, #myOverlay4").click(function () {
+        $("#myModal4").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay4").fadeOut(297);
+        });
+    });
 });
 
 function openCity(evt, cityName) {
@@ -112,11 +109,73 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+$(function () {
+    $("#tel-hide").hide(); // скрываем элементы
+    $(".show-tel").on("click", function () {
+        // указываем кнопку при клике на которую покажутся элементы
+        $(" #tel-hide").show(); // показываем элементы
+        appNormalise();
+    });
+});
 
-$(function() {
-$("#tel-hide").hide(); // скрываем элементы
-$(".show-tel").on('click', function() { // указываем кнопку при клике на которую покажутся элементы
-$(" #tel-hide").show(); // показываем элементы
- appNormalise();
- }) 
- });
+$(document).ready(function () {
+    $(".new_ad_price_category").change(function (event) {
+        console.log(
+            event.target.parentElement.getElementsByClassName(
+                "ads_input_fixed"
+            )[0]
+        );
+        console.log(
+            event.target.parentElement.getElementsByClassName(
+                "ads_input_lower"
+            )[0]
+        );
+        console.log(
+            event.target.parentElement.getElementsByClassName(
+                "ads_input_upper"
+            )[0]
+        );
+
+        switch (Number(event.target.value)) {
+            case 1:
+                console.log("TEST 1");
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_fixed"
+                )[0].style.display = "block";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_lower"
+                )[0].style.display = "none";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_upper"
+                )[0].style.display = "none";
+                break;
+            case 2:
+                console.log("TEST 2");
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_fixed"
+                )[0].style.display = "none";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_lower"
+                )[0].style.display = "none";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_upper"
+                )[0].style.display = "none";
+                break;
+            case 3:
+                console.log("TEST 3");
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_fixed"
+                )[0].style.display = "none";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_lower"
+                )[0].style.display = "block";
+                event.target.parentElement.getElementsByClassName(
+                    "ads_input_upper"
+                )[0].style.display = "block";
+                break;
+        }
+
+        console.log(event.target.value);
+        console.log(event);
+    });
+});
--- static/js/registrvalidate.js
+++ static/js/registrvalidate.js
@@ -1,26 +1,37 @@
 //login block
-const loginSubmit = document.querySelector("#loginSubmit")
-
+const loginSubmit = document.querySelector("#loginSubmit");
 
 // registration block
-const registrationFrom = document.querySelector("#myModal1 form")
-const registrationInputs = document.querySelectorAll("#myModal1 form input")
-const registrationSubmit = document.querySelector('#registration_submit');
-const registrationEmailField = document.querySelector('#registrationEmailField');
-const registrationEmailFeedBackArea = document.querySelector('.registrationEmailFeedBackArea');
-const passwordInputOne = document.querySelector('#passwordInputOne');
-const passwordInputTwo = document.querySelector('#passwordInputTwo');
-const passwordInputOneVal = document.querySelector('#passwordInputOne').value;
-const passwordInputTwoVal = document.querySelector('#passwordInputTwo').value;
-const registrationPasswordFeedBackArea = document.querySelector('.registrationPasswordFeedBackArea');
-const registrationPasswordFeedBackAreaOne = document.querySelector('.registrationPasswordFeedBackAreaOne');
-const formValidationFeedBack = document.querySelector('.formValidationFeedBack');
-const showPasswordToggleOne = document.querySelector('.showPasswordToggleOne');
-const showPasswordToggleTwo = document.querySelector('.showPasswordToggleTwo');
-
+const registrationFrom = document.querySelector("#myModal1 form");
+const registrationInputs = document.querySelectorAll("#myModal1 form input");
+const registrationSubmit = document.querySelector("#registration_submit");
+const registrationEmailField = document.querySelector(
+    "#registrationEmailField"
+);
+const registrationEmailFeedBackArea = document.querySelector(
+    ".registrationEmailFeedBackArea"
+);
+const passwordInputOne = document.querySelector("#passwordInputOne");
+const passwordInputTwo = document.querySelector("#passwordInputTwo");
+const passwordInputOneVal = document.querySelector("#passwordInputOne").value;
+const passwordInputTwoVal = document.querySelector("#passwordInputTwo").value;
+const registrationPasswordFeedBackArea = document.querySelector(
+    ".registrationPasswordFeedBackArea"
+);
+const registrationPasswordFeedBackAreaOne = document.querySelector(
+    ".registrationPasswordFeedBackAreaOne"
+);
+const formValidationFeedBack = document.querySelector(
+    ".formValidationFeedBack"
+);
+const showPasswordToggleOne = document.querySelector(".showPasswordToggleOne");
+const showPasswordToggleTwo = document.querySelector(".showPasswordToggleTwo");
 
 const handleToggleInputOne = (e) => {
-    if (showPasswordToggleOne.className === "showPasswordToggleOne password-control view") {
+    if (
+        showPasswordToggleOne.className ===
+        "showPasswordToggleOne password-control view"
+    ) {
         passwordInputOne.setAttribute("type", "password");
     } else {
         passwordInputOne.setAttribute("type", "text");
@@ -28,7 +39,10 @@ const handleToggleInputOne = (e) => {
 };
 
 const handleToggleInputTwo = (e) => {
-    if (showPasswordToggleTwo.className === "showPasswordToggleTwo password-control view") {
+    if (
+        showPasswordToggleTwo.className ===
+        "showPasswordToggleTwo password-control view"
+    ) {
         passwordInputTwo.setAttribute("type", "password");
     } else {
         passwordInputTwo.setAttribute("type", "text");
@@ -38,47 +52,49 @@ const handleToggleInputTwo = (e) => {
 showPasswordToggleOne.addEventListener("click", handleToggleInputOne);
 showPasswordToggleTwo.addEventListener("click", handleToggleInputTwo);
 
-
-let validEmail = function() {
+let validEmail = function () {
     let form = document.querySelector("#registrationForm");
-    let registrationEmailField = document.querySelector("#registrationEmailField");
-    let registrationEmailFeedBackArea = document.querySelector(".registrationEmailFeedBackArea");
+    let registrationEmailField = document.querySelector(
+        "#registrationEmailField"
+    );
+    let registrationEmailFeedBackArea = document.querySelector(
+        ".registrationEmailFeedBackArea"
+    );
     let emailVal = form.elements.registrationEmailField.value;
 
     if (emailVal.length >= 0) {
-
-        fetch("validate-email/", {
-            body: JSON.stringify({ email: emailVal }),
-            method: "POST",
-        })
-
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.email_error) {
-                registrationEmailField.classList.add("is-invalid");
-                registrationEmailFeedBackArea.style.display = "block";
-                registrationEmailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
-            } else {
-                registrationEmailField.classList.remove("is-invalid");
-                registrationEmailFeedBackArea.style.display = "none";
-
-            }
-            if (data.email_valid) {
-                return true;
-            }
-        });
-
+        axios
+            .post("/validate-email/", { email: emailVal })
+            .then((res) => {
+                console.log(res);
+                if (res.status === 200) {
+                    registrationEmailField.classList.remove("is-invalid");
+                    registrationEmailFeedBackArea.style.display = "none";
+                }
+            })
+            .catch((err) => {
+                console.log(err.response);
+                if (err.response.data.email_error) {
+                    registrationEmailField.classList.add("is-invalid");
+                    registrationEmailFeedBackArea.style.display = "block";
+                    registrationEmailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
+                }
+            });
     }
 };
 
-let validPassword = function() {
+let validPassword = function () {
     let form = document.querySelector("#registrationForm");
-    let passwordInputOne = document.querySelector('#passwordInputOne');
-    let passwordInputTwo = document.querySelector('#passwordInputTwo');
+    let passwordInputOne = document.querySelector("#passwordInputOne");
+    let passwordInputTwo = document.querySelector("#passwordInputTwo");
     let passwordOneVal = form.elements.passwordInputOne.value;
     let passwordTwoVal = form.elements.passwordInputTwo.value;
-    let registrationPasswordFeedBackArea = document.querySelector('.registrationPasswordFeedBackArea');
-    let registrationPasswordsFeedBackArea = document.querySelector('.registrationPasswordsFeedBackArea')
+    let registrationPasswordFeedBackArea = document.querySelector(
+        ".registrationPasswordFeedBackArea"
+    );
+    let registrationPasswordsFeedBackArea = document.querySelector(
+        ".registrationPasswordsFeedBackArea"
+    );
 
     if (passwordOneVal == "" || passwordTwoVal == "") {
         passwordInputOne.classList.add("is-invalid");
@@ -95,22 +111,27 @@ let validPassword = function() {
             passwordInputOne.classList.add("is-invalid");
             passwordInputTwo.classList.add("is-invalid");
             registrationPasswordFeedBackArea.innerHTML = `<p>Пароли не совпадают</p>`;
-            } else if (passwordOneVal === passwordTwoVal && passwordOneVal.length >= 6) {
-                passwordInputOne.classList.remove("is-invalid");
-                passwordInputTwo.classList.remove("is-invalid");
-                registrationPasswordFeedBackArea.style.display = "none";
-                return true;
-                } else {
-                    registrationPasswordFeedBackArea.innerHTML = `<p>Пароль должен быть не менее 6 символов</p>`;
-                }
+        } else if (
+            passwordOneVal === passwordTwoVal &&
+            passwordOneVal.length >= 6
+        ) {
+            passwordInputOne.classList.remove("is-invalid");
+            passwordInputTwo.classList.remove("is-invalid");
+            registrationPasswordFeedBackArea.style.display = "none";
+            return true;
+        } else {
+            registrationPasswordFeedBackArea.innerHTML = `<p>Пароль должен быть не менее 6 символов</p>`;
+        }
     }
 };
 
-let validCheck = function() {
+let validCheck = function () {
     let form = document.querySelector("#registrationForm");
-    let checkAgree = document.querySelector('#is_agree')
-    let check18Over = document.querySelector('#is_18over')
-    let formValidationFeedBack = document.querySelector('.formValidationFeedBack');
+    let checkAgree = document.querySelector("#is_agree");
+    let check18Over = document.querySelector("#is_18over");
+    let formValidationFeedBack = document.querySelector(
+        ".formValidationFeedBack"
+    );
     if (!checkAgree.checked || !check18Over.checked) {
         formValidationFeedBack.style.display = "block";
         formValidationFeedBack.innerHTML = `<p>Подтвердите свое согласие и что Вам больше 18 лет</p>`;
@@ -125,8 +146,8 @@ registrationSubmit.onclick = (e) => {
     let emailVal = form.elements.registrationEmailField.value;
     let passwordOneVal = form.elements.passwordInputOne.value;
     let passwordTwoVal = form.elements.passwordInputTwo.value;
-    let checkAgree = document.querySelector('#is_agree')
-    let check18Over = document.querySelector('#is_18over')
+    let checkAgree = document.querySelector("#is_agree");
+    let check18Over = document.querySelector("#is_18over");
 
     e.preventDefault();
     validEmail();
@@ -134,23 +155,30 @@ registrationSubmit.onclick = (e) => {
     validCheck();
 
     if (validCheck() && validPassword()) {
-        fetch("register/", {
-           body: JSON.stringify({ email: emailVal,
-                                  password1: passwordOneVal,
-                                  password2: passwordTwoVal,
-                                  is_agree: checkAgree.checked,
-                                  is_over18: check18Over.checked,
-           }),
-           method: "POST",
-        })
-
-        .then((res) => res.json())
-            .then((data) => {
-
-            });
+        axios
+            .post("/register/", {
+                email: emailVal,
+                password1: passwordOneVal,
+                password2: passwordTwoVal,
+                is_agree: checkAgree.checked,
+                is_over18: check18Over.checked,
+            })
+            .then((res) => {
+                if (res.status === 200) {
+                    $("#myModal1").animate({ opacity: 0 }, 198, function () {
+                        $(this).css("display", "none");
+                        $("#myOverlay1").fadeOut(297);
+                    });
+                    $("#myOverlay4").fadeIn(297, function () {
+                        $("#myModal4")
+                            .css("display", "block")
+                            .animate({ opacity: 1 }, 198);
+                    });
+                } else console.log(res);
+            })
+            .catch((err) => console.log(err.response));
     }
 };
-
 
 //login block
 loginSubmit.onclick = (e) => {
@@ -162,42 +190,55 @@ loginSubmit.onclick = (e) => {
     let loginPasswordVal = loginForm.elements.loginPassword.value;
     let loginRememberMe = document.querySelector("#loginRememberMe");
     let loginRememberMeVal = loginForm.elements.loginRememberMe.value;
-    let loginEmailFeedBackArea = document.querySelector(".loginEmailFeedBackArea");
-    let loginPasswordFeedBackArea = document.querySelector(".loginPasswordFeedBackArea");
-
-    if (loginEmailVal.length > 0) {
-        fetch("login/", {
-            body: JSON.stringify({ email: loginEmailVal,
-                                   password: loginPasswordVal,
-                                   remember: loginRememberMe.checked,
-                                   }),
-            method: "POST",
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.email_error) {
-                loginEmail.classList.add("is-invalid");
-                loginPassword.classList.add("is-invalid");
-                loginEmailFeedBackArea.style.display = "block";
-                loginPasswordFeedBackArea.style.display = "block";
-
-                loginEmailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
-                loginPasswordFeedBackArea.innerHTML = `<p>${data.password_error}</p>`;
-            } else {
-                loginEmail.classList.remove("is-invalid");
-                loginPassword.classList.remove("is-invalid");
-                loginEmailFeedBackArea.style.display = "none";
-                loginPasswordFeedBackArea.style.display = "none";
-            }
-        });
-    } else {
+    let loginEmailFeedBackArea = document.querySelector(
+        ".loginEmailFeedBackArea"
+    );
+    let loginPasswordFeedBackArea = document.querySelector(
+        ".loginPasswordFeedBackArea"
+    );
+    if ((loginEmailVal.length = 0)) {
         loginEmail.classList.add("is-invalid");
         loginPassword.classList.add("is-invalid");
+        loginEmailFeedBackArea.style.display = "block";
         loginPasswordFeedBackArea.style.display = "block";
-        loginPasswordFeedBackArea.innerHTML = `<p>Заполните все поля!</p>`;
+        loginEmailFeedBackArea.innerHTML = `<p>Введите Ваш E-mail адрес</p>`;
+        loginPasswordFeedBackArea.innerHTML = `<p>Введите Ваш пароль</p>`;
+    } else {
+        loginEmail.classList.remove("is-invalid");
+        loginPassword.classList.remove("is-invalid");
+        loginEmailFeedBackArea.style.display = "none";
+        loginPasswordFeedBackArea.style.display = "none";
     }
+    if (loginEmailVal.length > 0) {
+        axios
+            .post("/login/", {
+                email: loginEmailVal,
+                password: loginPasswordVal,
+                remember: loginRememberMe.checked,
+            })
+            .then((res) => {
+                if (res.status === 200) {
+                    loginEmail.classList.remove("is-invalid");
+                    loginPassword.classList.remove("is-invalid");
+                    loginEmailFeedBackArea.style.display = "none";
+                    loginPasswordFeedBackArea.style.display = "none";
+                    window.location.reload();
+                } else console.log(res);
+            })
+            .catch((err) => {
+                if (err.response.data.email_error) {
+                    loginEmail.classList.add("is-invalid");
+                    loginEmailFeedBackArea.style.display = "block";
+                    loginEmailFeedBackArea.innerHTML = `<p>${err.response.data.email_error}</p>`;
+                }
 
+                if (err.response.data.password_error) {
+                    loginPassword.classList.add("is-invalid");
+                    loginPasswordFeedBackArea.style.display = "block";
+                    loginPasswordFeedBackArea.innerHTML = `<p>${err.response.data.password_error}</p>`;
+                }
+
+                console.log(err.response);
+            });
+    }
 };
-
-//Отключаем кнопку верификации телефона
-document.getElementById("phoneVerify").disabled = true;

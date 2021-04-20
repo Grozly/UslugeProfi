edf6a44f54f62467365770080223e0ca8b6ba021
Johudo <dimazuev11@gmail.com>
2021-04-21 00:22:43 +0300

    Added price in new ad form

--- static/js/createNewAd.js
+++ static/js/createNewAd.js
@@ -1,16 +1,28 @@
-let adName = document.querySelector("#id_name").value;
+var adName = document.querySelector("#id_name").value;
 
 document.querySelector("#create-new-ad").addEventListener("click", (event) => {
     event.preventDefault();
 
-    let adName = document.querySelector("#id_name").value;
-    let adDescription = document.querySelector("#id_description").value;
-    let adImageFile = document.querySelector("#id_photo_announcement").files[0];
-    let adCategoty = document.querySelector("#select_category").value;
-    let adSubcategory = document.querySelector("#select_subcategory").value;
-    let adAddress = document.querySelector("#id_address").value;
+    var adName = document.querySelector("#id_name").value;
+    var adDescription = document.querySelector("#id_description").value;
+    var adImageFile = document.querySelector("#id_photo_announcement").files[0];
+    var adCategoty = document.querySelector("#select_category").value;
+    var adSubcategory = document.querySelector("#select_subcategory").value;
+    var adAddress = document.querySelector("#id_address").value;
 
-    let formData = new FormData();
+    var fixedPrice = document.querySelector("#new_ad_fixed_price")
+        ? document.querySelector("#new_ad_fixed_price").value
+        : null;
+
+    var lowerPrice = document.querySelector("#new_ad_lower_price")
+        ? document.querySelector("#new_ad_lower_price").value
+        : null;
+
+    var upperPrice = document.querySelector("#new_ad_upper_price")
+        ? document.querySelector("#new_ad_upper_price").value
+        : null;
+
+    var formData = new FormData();
 
     formData.set("name", adName);
     formData.set("description", adDescription);
@@ -18,6 +30,12 @@ document.querySelector("#create-new-ad").addEventListener("click", (event) => {
     formData.set("categoty", adCategoty);
     formData.set("subcategory", adSubcategory);
     formData.set("address", adAddress);
+
+    if (fixedPrice) formData.set("price", fixedPrice);
+    if (lowerPrice && upperPrice) {
+        formData.set("lower_price", lowerPrice);
+        formData.set("upper_price", upperPrice);
+    }
 
     axios.post("/create-ad/", formData, {
         headers: {

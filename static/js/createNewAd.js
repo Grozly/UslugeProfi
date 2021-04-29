5ffcaed87d408f06e533ba6906680bf8d0761390
Johudo <dimazuev11@gmail.com>
2021-04-29 14:35:59 +0300

    Fixed ad creator

--- static/js/createNewAd.js
+++ static/js/createNewAd.js
@@ -6,7 +6,6 @@ const adCategoty = document.querySelector("#select_category");
 const adSubcategory = document.querySelector("#select_subcategory");
 const subcategoryText = document.getElementById("subcategory_text");
 const adAddress = document.getElementById("id_address");
-const adOptions = document.querySelectorAll(".new_ad_options");
 const adImageFile = document.getElementById("id_photo_announcement");
 const userID = document.getElementsByName("user_id")[0];
 const csrf = document.getElementsByName("csrfmiddlewaretoken");
@@ -38,6 +37,9 @@ form.addEventListener("submit", (event) => {
 
     const optionsArray = [];
 
+    const adOptions = document.querySelectorAll(".new_ad_options");
+    console.log(adOptions);
+
     adOptions.forEach((item, index) => {
         const fixedPrice = item.getElementsByClassName("ads_input_fixed")[0];
         const lowerPrice = item.getElementsByClassName("ads_input_lower")[0];
@@ -57,7 +59,6 @@ form.addEventListener("submit", (event) => {
         )[0];
 
         const optionID = item.getAttribute("data-id");
-
         if (checkbox.checked) {
             switch (Number(priceSelector.value)) {
                 case 1:
@@ -116,62 +117,189 @@ form.addEventListener("submit", (event) => {
 });
 
 $.ajax({
-        type: "GET",
-        url: `/ajax/category-val/`,
-        success: function (response) {
-            console.log(response.data);
-            const categoryData = response.data
-            categoryData.map(item=>{
-                const option = document.createElement('option')
-                option.textContent = item.name
-                option.setAttribute('class', 'item')
-                option.setAttribute('data-value', item.name)
-                adCategoty.appendChild(option)
-            })
-        },
-        error: function (error) {
-            console.log(error);
-        }
+    type: "GET",
+    url: `/ajax/category-val/`,
+    success: function (response) {
+        console.log(response.data);
+        const categoryData = response.data;
+        categoryData.map((item) => {
+            const option = document.createElement("option");
+            option.textContent = item.name;
+            option.setAttribute("class", "item");
+            option.setAttribute("data-value", item.name);
+            adCategoty.appendChild(option);
+        });
+    },
+    error: function (error) {
+        console.log(error);
+    },
 });
 
-adCategoty.addEventListener('change', e=>{
-    const selectedCategory = e.target.selectedIndex
+adCategoty.addEventListener("change", (e) => {
+    const selectedCategory = e.target.selectedIndex;
 
-    adSubcategory.innerHTML = ""
-    subcategoryText.textContent = "-- Выберите подкатегорию --"
+    adSubcategory.innerHTML = "";
+    subcategoryText.textContent = "-- Выберите подкатегорию --";
 
     $.ajax({
         type: "GET",
         url: `/ajax/subcategory-val/${selectedCategory}/`,
         success: function (response) {
-            const subcategoryData = response.data
-            subcategoryData.map(item=>{
-                const option = document.createElement('option')
-                option.textContent = item.name
-                option.setAttribute('class', 'item')
-                option.setAttribute('data-value', item.name)
-                adSubcategory.appendChild(option)
-            })
-            },
+            console.log(response.data);
+            adSubcategory.innerHTML = `
+                <option selected disabled="True" id="subcategory_text">
+                    -- Выберите подкатегорию --
+                </option>`;
+
+            var adsBlock = document.getElementById("ads_by_subcategory_block");
+            adsBlock.innerHTML = "";
+
+            response.data.forEach((item, index) => {
+                adSubcategory.innerHTML =
+                    adSubcategory.innerHTML +
+                    `<option value="${item.id}">
+                       ${item.name}
+                   </option>`;
+            });
+        },
         error: function (error) {
             console.log(error);
-        }
+        },
     });
-
 });
 
-adSubcategory.addEventListener("change", e=>{
-    const selectedSubcategory = e.target.selectedIndex
+adSubcategory.addEventListener("change", (e) => {
+    const selectedSubcategory = e.target.selectedIndex;
 
     $.ajax({
         type: "GET",
         url: `/ajax/service-val/${selectedSubcategory}/`,
         success: function (response) {
             console.log(response.data);
+            var adsBlock = document.getElementById("ads_by_subcategory_block");
+            adsBlock.innerHTML = "";
+
+            response.data.forEach((item, index) => {
+                adsBlock.innerHTML =
+                    adsBlock.innerHTML +
+                    `<div class="ads_options new_ad_options" data-id=${item.id} >
+                        <div>
+                            <input
+                                class="subcat_checkbox"
+                                type="checkbox"
+                                name="option1"
+                                value="${item.is_active}"
+                            />
+                            ${item.name}
+                            <br />
+                        </div>
+                        <select id="select_price_${index}" size="1" name="subcategory" class="new_ad_price_category select_price">
+                            <option disabled>Цена</option>
+                            <option selected value="1">Цена</option>
+                            <option value="2">Договорная</option>
+                            <option value="3">Диапазон</option>
+                        </select>
+
+                        <div class="ads_input_flex">
+                            <input
+                                id="new_ad_fixed_price_${index}"
+                                type="text"
+                                class="ads_input ads_input_fixed"
+                                placeholder="Цена"
+                                style="display: block"
+                            />
+                            <input
+                                id="new_ad_lower_price_${index}"
+                                type="text"
+                                class="ads_input ads_input_lower"
+                                placeholder="Цена от"
+                                style="display: none"
+                            />
+                            <input
+                                id="new_ad_upper_price_${index}"
+                                type="text"
+                                class="ads_input ads_input_upper"
+                                placeholder="Цена до"
+                                style="display: none"
+                            />
+                        </div>
+                        <select id="select_currency_${index}" size="1" name="subcategory" class="select_currency">
+                            <option disabled>Валюта</option>
+                            <option value="1">дин</option>
+                            <option selected value="2">руб</option>
+                            <option value="3">доллар</option>
+                        </select>
+                        <select id="select_measurement_${index}" size="1" name="subcategory" class="select_measurement">
+                            <option value="1">шт</option>
+                            <option selected value="2">м2</option>
+                            <option value="3">кг</option>
+                        </select>
+                    </div>`;
+            });
+
+            $(".new_ad_price_category").change(function (event) {
+                console.log(
+                    event.target.parentElement.getElementsByClassName(
+                        "ads_input_fixed"
+                    )[0]
+                );
+                console.log(
+                    event.target.parentElement.getElementsByClassName(
+                        "ads_input_lower"
+                    )[0]
+                );
+                console.log(
+                    event.target.parentElement.getElementsByClassName(
+                        "ads_input_upper"
+                    )[0]
+                );
+
+                switch (Number(event.target.value)) {
+                    case 1:
+                        console.log("TEST 1");
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_fixed"
+                        )[0].style.display = "block";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_lower"
+                        )[0].style.display = "none";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_upper"
+                        )[0].style.display = "none";
+                        break;
+                    case 2:
+                        console.log("TEST 2");
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_fixed"
+                        )[0].style.display = "none";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_lower"
+                        )[0].style.display = "none";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_upper"
+                        )[0].style.display = "none";
+                        break;
+                    case 3:
+                        console.log("TEST 3");
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_fixed"
+                        )[0].style.display = "none";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_lower"
+                        )[0].style.display = "block";
+                        event.target.parentElement.getElementsByClassName(
+                            "ads_input_upper"
+                        )[0].style.display = "block";
+                        break;
+                }
+
+                console.log(event.target.value);
+                console.log(event);
+            });
         },
         error: function (error) {
             console.log(error);
-        }
+        },
     });
 });
 

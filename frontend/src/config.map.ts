44f6a9c648d69379312bd9494921b5d683bbd491
Dmitriy <dimazuev11@gmail.com>
2021-06-22 17:28:51 +0300

    Update config.map.ts

--- frontend/src/config.map.ts
+++ frontend/src/config.map.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_TOKEN = "AIzaSyDpQh1_D9uH64pyTLhI3UnFph679MGPZ6Q";
+export const GOOGLE_MAPS_API_TOKEN = "";
 
 export const MAP_CHANGE_LOCATION_TIMEOUT = 800;
 export const MAP_DEFAULT_POSITION = { lat: -34.397, lng: 150.644 };

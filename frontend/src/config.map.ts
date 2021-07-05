9cf421dc4b578dce529efe28f34447c81586e9ab
Vadim Rusakov <rusakov.temp@mail.ru>
2021-07-05 15:17:13 +0500

    'new_front'

--- frontend/src/config.map.ts
+++ frontend/src/config.map.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_TOKEN = "";
+export const GOOGLE_MAPS_API_TOKEN = "AIzaSyD5pjku_bTWdlOZJGw6FF_vic6z_MjGgnI";
 
 export const MAP_CHANGE_LOCATION_TIMEOUT = 800;
 export const MAP_DEFAULT_POSITION = { lat: -34.397, lng: 150.644 };
